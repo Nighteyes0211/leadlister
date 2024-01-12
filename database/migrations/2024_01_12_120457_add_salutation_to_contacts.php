73bdd8fd751f03b74b6d03854eb0967f47757855
ISMAIL REHMAN <ismailrehman0331@gmail.com>
2024-01-12 17:33:54 +0500

    Add default salutation to contact migration

--- database/migrations/2024_01_12_120457_add_salutation_to_contacts.php
+++ database/migrations/2024_01_12_120457_add_salutation_to_contacts.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enum\Contact\SalutationEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('contacts', function (Blueprint $table) {
-            $table->string('salutation');
+            $table->string('salutation')->default(SalutationEnum::NONE->value);
         });
     }
 
